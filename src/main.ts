abbb14f1e35fe5601163301cdc58c18be8e934cc
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-06-29 16:35:42 -0300

    Yeah 💞

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,17 @@
 async function main() {
-  const response = await fetch("https://www.learnwithjason.dev/graphql", {
+  const response = await fetch("https://countries.trevorblades.com/graphql", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
       query: `
-          query GetLearnWithJasonEpisodes($now: DateTime!) {
-            allEpisode(limit: 10, sort: {date: ASC}, where: {date: {gte: $now}}) {
-              date
-              title
-              guest {
-                name
-                twitter
-              }
-              description
-            }
+        query {
+          countries {
+            capital
           }
+        }             
         `,
-      variables: {
-        now: new Date().toISOString(),
-      },
     }),
   }).then((res) => res.json());
 
