6c9b4efb4eeb98d65e6c120cd62a371ea3348d63
Nam Nguyen <nam.nguyen@shopify.com>
2024-03-09 19:34:29 -0400

    Add awesome wheel 💞

---
+++ src/spin.ts
@@ -0,0 +1,9 @@
+import "./spin.scss";
+
+const spinButton = document.getElementById("spin");
+const dial = document.querySelector(".dial");
+
+spinButton?.addEventListener("click", () => {
+  dial!.classList.toggle("spinning");
+  document.querySelector("content-container")!.classList.toggle("activated");
+});
