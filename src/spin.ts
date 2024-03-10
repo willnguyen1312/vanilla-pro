cb076a03ba4ca28b9bba4f5a73bee0c99cdc8f18
Nam Nguyen <nam.nguyen@shopify.com>
2024-03-09 20:03:22 -0400

    Add spin UI 🙈

--- src/spin.ts
+++ src/spin.ts
@@ -4,6 +4,6 @@ const spinButton = document.getElementById("spin");
 const dial = document.querySelector(".dial");
 
 spinButton?.addEventListener("click", () => {
-  dial!.classList.toggle("spinning");
-  document.querySelector("content-container")!.classList.toggle("activated");
+  dial?.classList.toggle("spinning");
+  document.querySelector("content-container")?.classList.toggle("activated");
 });
