a88b0642535d55592f2a3a87191f0dedd5489050
Nam Nguyen <nam.nguyen@shopify.com>
2025-03-24 11:10:38 -0600

    🤯

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,3 @@
 import "./main.css";
 
-// const button = document.getElementById("button") as HTMLButtonElement;
-
-// button.addEventListener("click", async () => {
-//   // Fetch a script.js file and place in the head of the document
-//   const script = document.createElement("script");
-//   script.src = "script2.js";
-//   document.head.appendChild(script);
-
-//   script.onload = async () => {
-//     const script = document.createElement("script");
-//     script.src = "script1.js";
-//     document.head.appendChild(script);
-//   };
-// });
+console.log("Hello, world!");
